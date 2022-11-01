d77b135360bbdc6c23906a88c52fbfccf8904235
Daniela Romero <danyrp45@gmail.com>
2022-11-01 17:16:34 -0600

    complete the crud in patients

---
+++ src/components/Error.jsx
@@ -0,0 +1,12 @@
+
+const Error = ({mensaje}) => {
+  return (
+    <div>
+      <p className="bg-red-700 text-white text-center p-3 uppercase font-bold mb-3 rounded">
+        {mensaje}
+      </p>
+    </div>
+  );
+};
+
+export default Error;
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,23 +1,44 @@
 
 
-const Paciente = () => {
+const Paciente = ({paciente, setPaciente, eliminarPaciente}) => {
+
+  const handleEliminar = ()=>{
+     const respuesta = confirm ("¿Deseas eliminar este paciente?")
+     if(respuesta){
+      eliminarPaciente(id)
+     }
+  }
+
+  const{nombre, propietario, email, fecha, sintomas, id}= paciente
   return (
-    <div className="m-3 px-5 py-10 rounded-xl bg-white shadow-md">
+    <div className="mx-5 my-10 px-5 py-10 rounded-xl bg-white shadow-md">
         <p className="font-bold mb-3 uppercase text-gray-700">
-          Nombre: <span className="font-normal normal-case">Hook</span>
+          Nombre: <span className="font-normal normal-case">{nombre}</span>
         </p>
         <p className="font-bold mb-3 uppercase text-gray-700">
-          Propietario: <span className="font-normal normal-case">Juan</span>
+          Propietario: <span className="font-normal normal-case">{propietario}</span>
         </p>
         <p className="font-bold mb-3 uppercase text-gray-700">
-          Email: <span className="font-normal normal-case">correo@correo.com</span>
+          Email: <span className="font-normal normal-case">{email}</span>
         </p>
         <p className="font-bold mb-3 uppercase text-gray-700">
-          Fecha Alta: <span className="font-normal normal-case">10 diciembre 2022</span>
+          Fecha Alta: <span className="font-normal normal-case">{fecha}</span>
         </p>
         <p className="font-bold mb-3 uppercase text-gray-700">
-          Síntomas: <span className="font-normal normal-case">Lorem ipsum</span>
+          Síntomas: <span className="font-normal normal-case">{sintomas}</span>
         </p>
+        <div className="flex justify-between mt-10">
+          <button 
+          type="button"
+          className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+          onClick={() => setPaciente(paciente) }
+          >Editar</button>
+          <button 
+          type="button"
+          className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+          onClick={handleEliminar}
+          >Eliminar</button>
+        </div>
       </div>
   )
 }
